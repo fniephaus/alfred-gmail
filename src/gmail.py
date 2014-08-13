99115b69871033c08b4fb88b55960ec03e5bb353
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-08-13 16:07:37 -0400

    Fix filter problems

--- src/gmail.py
+++ src/gmail.py
@@ -5,20 +5,12 @@ from workflow import Workflow, PasswordNotFound, MATCH_SUBSTRING
 from workflow.background import run_in_background, is_running
 
 
-def search_key_for_mail(mail):
-    elements = []
-    elements.append(mail['From'])
-    elements.append(mail['snippet'])
-    elements.append(mail['Subject'])
-    elements.append(mail['Date'])
-    return u' '.join(elements)
-
-
 def main(wf):
     if len(wf.args):
         query = wf.args[0]
     else:
         query = None
+
 
     if not wf.cached_data_fresh('gmail_list', max_age=3600):
         refresh_cache()
@@ -28,10 +20,6 @@ def main(wf):
         if len(item_list) == 0:
             wf.add_item('Your Gmail inbox is empty!', valid=False)
         else:
-            if query:
-                item_list = wf.filter(
-                    query, item_list, key=search_key_for_mail, match_on=MATCH_SUBSTRING)
-
             for index, item in enumerate(item_list):
                 name = item['From'][
                     :item['From'].find("<") - 1].replace('"', '')
@@ -39,7 +27,8 @@ def main(wf):
                 subtitle = '%s - %s' % (item['Date'][:-6], item['snippet'])
                 arg = '%s %s' % (item['thread_id'], item['id'])
 
-                wf.add_item(title, subtitle, arg=arg, valid=True)
+                if not query or query.lower() in ' '.join([title, subtitle]).lower():
+                    wf.add_item(title, subtitle, arg=arg, valid=True)
     else:
         wf.add_item("Could receive your emails.",
                     "Please try again or file a bug report!", valid=False)
