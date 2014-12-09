a8c4628724ad0a89231cbb55433fdbeb6d6ecc3e
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-12-09 10:23:12 +0100

    Refactor code and add icons

--- src/gmail.py
+++ src/gmail.py
@@ -17,11 +17,11 @@ WF = Workflow(update_settings={
 def main():
     if WF.update_available:
         subtitle = 'New: %s' % WF.update_info['body']
-        WF.add_item("An update is available!", subtitle, 
-            icon=get_icon("cloud-download"),
-            autocomplete='workflow:update',
-            valid=False
-        )
+        WF.add_item("An update is available!", subtitle,
+                    icon=get_icon("cloud-download"),
+                    autocomplete='workflow:update',
+                    valid=False
+                    )
 
     if len(WF.args):
         query = WF.args[0]
@@ -33,7 +33,8 @@ def main():
         thread_query = query[
             query.find(THREAD_DELIMITER) + len(THREAD_DELIMITER):].split()
         if len(thread_query) == 0:
-            WF.add_item("Missing Thread ID", icon=get_icon("alert"), valid=False)
+            WF.add_item(
+                "Missing Thread ID", icon=get_icon("alert"), valid=False)
             WF.send_feedback()
             return 0
 
@@ -43,14 +44,15 @@ def main():
             WF.add_item("Caching problem", icon=get_icon("alert"), valid=False)
             WF.send_feedback()
             return 0
-            
+
         selected_thread = None
         for thread in label_threads:
             if thread['threadId'] == thread_id:
                 selected_thread = thread
 
         if selected_thread is None:
-            WF.add_item("Invalid Thread ID", icon=get_icon("alert"), valid=False)
+            WF.add_item(
+                "Invalid Thread ID", icon=get_icon("alert"), valid=False)
             WF.send_feedback()
             return 0
 
@@ -65,10 +67,11 @@ def main():
                 'message': message
             })
             WF.add_item(message,
-                subtitle,
-                arg=arg,
-                valid=valid
-            )
+                        subtitle,
+                        icon=get_icon("pencil"),
+                        arg=arg,
+                        valid=valid
+                        )
         elif len(thread_query) > 1 and 'label' in thread_query[1]:
             label_list = WF.cached_data('gmail_labels', max_age=0)
             if label_list:
@@ -87,6 +90,7 @@ def main():
                         WF.add_item(
                             label['name'],
                             "Hit enter to add this label",
+                            icon=get_icon("tag"),
                             arg=arg,
                             valid=True)
                 else:
@@ -104,52 +108,8 @@ def main():
                     valid=False
                 )
         else:
-            email_title = selected_thread['Subject']
-            email_snippet = selected_thread['snippet']
-            WF.add_item(email_title, email_snippet, icon=get_icon("info"), arg=json.dumps({
-                'thread_id': thread_id,
-                'action': 'open',
-                'label': label,
-            }), valid=True)
-
-            WF.add_item("Mark As Read", icon=get_icon("mail-read"), arg=json.dumps({
-                'thread_id': thread_id,
-                'action': 'mark_as_read',
-                'query': query,
-            }), valid=True)
-
-            WF.add_item("Archive", icon=get_icon("package"), arg=json.dumps({
-                'thread_id': thread_id,
-                'action': 'archive_conversation',
-                'label': label,
-            }), valid=True)
-
-            if label != 'TRASH':
-                WF.add_item("Move To Trash", icon=get_icon("trashcan"), arg=json.dumps({
-                    'thread_id': thread_id,
-                    'action': 'trash_conversation',
-                    'label': label,
-                }), valid=True)
-
-            if label != 'INBOX':
-                WF.add_item("Move To Inbox", icon=get_icon("inbox"), arg=json.dumps({
-                    'thread_id': thread_id,
-                    'action': 'move_to_inbox',
-                    'label': label,
-                }), valid=True)
-
-            WF.add_item("Mark As Unread", icon=get_icon("mail"), arg=json.dumps({
-                'thread_id': thread_id,
-                'action': 'mark_as_unread',
-                'query': query,
-            }), valid=True)
-
-            WF.add_item("Quick Reply", icon=get_icon("pencil"), autocomplete='%s reply ' %
-                        query, valid=False)
-            WF.add_item("Add label", icon=get_icon("tag"), autocomplete='%s label ' %
-                        query, valid=False)
-            WF.add_item(
-                "...", icon=get_icon("mail-reply"), autocomplete=label, valid=False)
+            add_mail_actions(
+                selected_thread['Subject'], selected_thread['snippet'], label)
 
         WF.send_feedback()
         return 0
@@ -167,39 +127,20 @@ def main():
                 WF.add_item(
                     'No mails found!', icon=get_icon("alert"), arg="reopen", valid=True)
             else:
-                for index, item in enumerate(item_list):
-                    name = 'Unknown'
-                    if item['From']:
-                        name = item['From'][
-                            :item['From'].find("<") - 1].replace('"', '')
-                    if item['messages_count'] > 1:
-                        title = '%s (%s): %s' % (
-                            name, item['messages_count'], item['Subject'])
-                    else:
-                        title = '%s: %s' % (
-                            name, item['Subject'])
-                    icon = get_icon("mail") if item[
-                        'unread'] else get_icon("mail-read")
-                    subtitle = '%s - %s' % (item['Date'], item['snippet'])
-                    autocomplete = '%s %s%s' % (
-                        label, THREAD_DELIMITER, item['threadId'])
-
-                    if label_query.lower() in ' '.join([title, subtitle]).lower():
-                        WF.add_item(
-                            title,
-                            subtitle,
-                            icon=icon,
-                            autocomplete=autocomplete,
-                            valid=False
-                        )
+                add_mails(item_list, label, label_query)
+                
             WF.add_item(
-                "...", icon=get_icon("mail-reply"), arg="reopen", valid=True)
+                "...",
+                icon=get_icon("mail-reply"),
+                arg="reopen",
+                valid=True
+            )
         else:
             WF.add_item("Could receive your emails.",
-                "Please try again or file a bug report!",
-                icon=get_icon("alert"),
-                valid=False
-            )
+                        "Please try again or file a bug report!",
+                        icon=get_icon("alert"),
+                        valid=False
+                        )
 
         WF.send_feedback()
         return 0
@@ -214,6 +155,80 @@ def main():
         background_refresh()
 
     WF.send_feedback()
+
+
+def add_mails(item_list, label, label_query):
+    for index, item in enumerate(item_list):
+        name = 'Unknown'
+        if item['From']:
+            name = item['From'][
+                :item['From'].find("<") - 1].replace('"', '')
+        if item['messages_count'] > 1:
+            title = '%s (%s): %s' % (
+                name, item['messages_count'], item['Subject'])
+        else:
+            title = '%s: %s' % (
+                name, item['Subject'])
+        icon = get_icon("mail") if item[
+            'unread'] else get_icon("mail-read")
+        subtitle = '%s - %s' % (item['Date'], item['snippet'])
+        autocomplete = '%s %s%s' % (
+            label, THREAD_DELIMITER, item['threadId'])
+
+        if label_query.lower() in ' '.join([title, subtitle]).lower():
+            WF.add_item(
+                title,
+                subtitle,
+                icon=icon,
+                autocomplete=autocomplete,
+                valid=False
+            )
+
+def add_mail_actions(title, snippet, label):
+    WF.add_item(title, snippet, icon=get_icon("info"), arg=json.dumps({
+        'thread_id': thread_id,
+        'action': 'open',
+        'label': label,
+    }), valid=True)
+
+    WF.add_item("Mark As Read", icon=get_icon("mail-read"), arg=json.dumps({
+        'thread_id': thread_id,
+        'action': 'mark_as_read',
+        'query': query,
+    }), valid=True)
+
+    WF.add_item("Archive", icon=get_icon("package"), arg=json.dumps({
+        'thread_id': thread_id,
+        'action': 'archive_conversation',
+        'label': label,
+    }), valid=True)
+
+    if label != 'TRASH':
+        WF.add_item("Move To Trash", icon=get_icon("trashcan"), arg=json.dumps({
+            'thread_id': thread_id,
+            'action': 'trash_conversation',
+            'label': label,
+        }), valid=True)
+
+    if label != 'INBOX':
+        WF.add_item("Move To Inbox", icon=get_icon("inbox"), arg=json.dumps({
+            'thread_id': thread_id,
+            'action': 'move_to_inbox',
+            'label': label,
+        }), valid=True)
+
+    WF.add_item("Mark As Unread", icon=get_icon("mail"), arg=json.dumps({
+        'thread_id': thread_id,
+        'action': 'mark_as_unread',
+        'query': query,
+    }), valid=True)
+
+    WF.add_item("Quick Reply", icon=get_icon("pencil"), autocomplete='%s reply ' %
+                query, valid=False)
+    WF.add_item("Add label", icon=get_icon("tag"), autocomplete='%s label ' %
+                query, valid=False)
+    WF.add_item(
+        "...", icon=get_icon("mail-reply"), autocomplete=label, valid=False)
 
 
 def get_icon(name):
